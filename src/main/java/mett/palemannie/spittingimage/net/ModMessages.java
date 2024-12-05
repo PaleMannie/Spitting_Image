03e17c8c1aef3e3fa5013a819eea1d4bec099df0
PaleMannie <einalex44@gmail.com>
2024-12-05 22:51:42 +0100

    Port 1.20.2 done

--- src/main/java/mett/palemannie/spittingimage/net/ModMessages.java
+++ src/main/java/mett/palemannie/spittingimage/net/ModMessages.java
@@ -3,37 +3,31 @@ package mett.palemannie.spittingimage.net;
 import mett.palemannie.spittingimage.SpittingImage;
 import mett.palemannie.spittingimage.net.packets.SpitC2SPacket;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.*;
 
 public class ModMessages {
-    private static SimpleChannel INSTANCE;
     private static int PacketID = 0;
     private static int id(){
         return PacketID++;
     }
     final static int version = 1;
 
+    private static final SimpleChannel INSTANCE = ChannelBuilder.named(new ResourceLocation(SpittingImage.MODID, "messages"))
+            .networkProtocolVersion(version)
+            .clientAcceptedVersions(((status, version1) -> true))
+            .serverAcceptedVersions(((status, version1) -> true))
+            .simpleChannel();
+
     public static void register(){
-        SimpleChannel net = ChannelBuilder.named(new ResourceLocation(SpittingImage.MODID, "messages"))
-                .networkProtocolVersion(version).clientAcceptedVersions(s -> true).serverAcceptedVersions(s -> true)
-                .simpleChannel();
-
-        INSTANCE = net;
-
-        net.messageBuilder(SpitC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+        INSTANCE.messageBuilder(SpitC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
                 .decoder(SpitC2SPacket::new)
-                .encoder(SpitC2SPacket::toBytes)
+                .encoder(SpitC2SPacket::encode)
                 .consumerMainThread(SpitC2SPacket::handle)
                 .add();
-
     }
 
-    public static <MSG> void sendToServer(MSG message){
-        INSTANCE.sendToServer(message);
+    public static void sendToServer(Object message){
+        INSTANCE.send(message, PacketDistributor.SERVER.noArg());
     }
 
-    public static <MSG> void sendToPlayer(MSG message, ServerPlayer player){
-        INSTANCE.send(PacketDistributor.PLAYER.with(()->player), message);
-    }
 }
--- src/main/java/mett/palemannie/spittingimage/net/packets/SpitC2SPacket.java
+++ src/main/java/mett/palemannie/spittingimage/net/packets/SpitC2SPacket.java
@@ -4,24 +4,19 @@ import mett.palemannie.spittingimage.server.ServerPlayHandler;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.network.CustomPayloadEvent;
-import net.minecraftforge.network.NetworkEvent;
-
-import java.util.function.Supplier;
 
 public class SpitC2SPacket {
     public SpitC2SPacket(){
     }
     public SpitC2SPacket(FriendlyByteBuf buf){
     }
-    public void toBytes(FriendlyByteBuf buf){
+    public void encode(FriendlyByteBuf buf){
     }
 
-    public boolean handle(Supplier<CustomPayloadEvent.Context> supplier){
-
-        CustomPayloadEvent.Context context = supplier.get();
+    public boolean handle(CustomPayloadEvent.Context context){
         context.enqueueWork(()-> {
-
             ServerPlayer player = context.getSender();
+
             if(player == null) return;
             if(player.isSpectator()) return;
 
