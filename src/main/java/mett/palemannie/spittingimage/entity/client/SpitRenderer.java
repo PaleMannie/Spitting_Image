684b4386ad82ee71593cce9c5d93872283df7e8c
PaleMannie <einalex44@gmail.com>
2025-06-16 19:08:22 +0200

    Version 4 port

--- src/main/java/mett/palemannie/spittingimage/entity/client/SpitRenderer.java
+++ src/main/java/mett/palemannie/spittingimage/entity/client/SpitRenderer.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import mett.palemannie.spittingimage.SpittingImage;
 import mett.palemannie.spittingimage.entity.custom.SpitEntity;
+import mett.palemannie.spittingimage.util.SpittingImageConfig;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -25,18 +26,21 @@ public class SpitRenderer extends EntityRenderer<SpitEntity, LlamaSpitRenderStat
 
     public void render(LlamaSpitRenderState pRenderState, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight) {
 
-        pPoseStack.pushPose();
+        if(SpittingImageConfig.COMMON.spitModel.get()){
+            pPoseStack.pushPose();
 
-        pPoseStack.translate(0f, 0.1f, 0f);
+            pPoseStack.translate(0f, 0.1f, 0f);
 
-        pPoseStack.mulPose(Axis.YP.rotationDegrees(pRenderState.yRot));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(-pRenderState.xRot + 180f));
+            pPoseStack.mulPose(Axis.YP.rotationDegrees(pRenderState.yRot));
+            pPoseStack.mulPose(Axis.XP.rotationDegrees(-pRenderState.xRot + 180f));
 
-        this.model.setupAnim(pRenderState);
-        VertexConsumer vertexconsumer = pBufferSource.getBuffer(this.model.renderType(SPIT_LOCATION));
-        this.model.renderToBuffer(pPoseStack, vertexconsumer, pPackedLight, OverlayTexture.NO_OVERLAY);
-        pPoseStack.popPose();
-        super.render(pRenderState, pPoseStack, pBufferSource, pPackedLight);
+            this.model.setupAnim(pRenderState);
+            VertexConsumer vertexconsumer = pBufferSource.getBuffer(this.model.renderType(SPIT_LOCATION));
+            this.model.renderToBuffer(pPoseStack, vertexconsumer, pPackedLight, OverlayTexture.NO_OVERLAY);
+            pPoseStack.popPose();
+
+            super.render(pRenderState, pPoseStack, pBufferSource, pPackedLight);
+        }
     }
 
     public LlamaSpitRenderState createRenderState() {
